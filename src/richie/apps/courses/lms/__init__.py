e2bc5cfc22ec5af1794d6d2029f2d305074ae434
Mehdi Benadda <me@mbenadda.com>
2020-07-17 11:46:54 +0200

    ✨(enrollment) add a filter to check if a course has managed enrollments
    We have all the logic in place to manage enrollments for courses through
    a frontend component and API endpoints it can connect to. The last
    missing piece is incorporating the frontend component in the template,
    for the course runs (and Richie installs) that need it.
    
    Rather than force everything to behave the same way through a Richie-wide
    setting, we've chosen to add a filter that enables each course run
    template to check for a matching LMS. That way, courses with managed
    enrollments can coexist on a single Richie instance with courses that
    simply provide a link to an external LMS.
    
    For Richie instances with no managed enrollments, the filter will simply
    always return False and default to the "link to external LMS" approach.

--- src/richie/apps/courses/lms/__init__.py
+++ src/richie/apps/courses/lms/__init__.py
@@ -15,13 +15,19 @@ class LMSHandler:
 
     @staticmethod
     def select_lms(url):
-        """Select and return the first LMS backend matching the url passed in argument."""
+        """
+        Select and return the first LMS backend matching the url passed in argument.
+
+        Default to None if no LMS is found to enable use-cases where we need to detect whether
+        a course run has a matching LMS or not. Callers can determine if not finding an LMS
+        backend is an exception or not.
+        """
 
         for lms_configuration in settings.LMS_BACKENDS:
             if re.match(lms_configuration.get("SELECTOR_REGEX", r".*"), url):
                 return import_string(lms_configuration["BACKEND"])(lms_configuration)
 
-        raise ImproperlyConfigured()
+        return None
 
     @classmethod
     def get_enrollment(cls, user, url):
@@ -29,7 +35,12 @@ class LMSHandler:
         Get enrollment statuc for a user on a course run given its url. This method is a wrapper
         that routes the request to the LMS linked to this LMS.
         """
-        return cls.select_lms(url).get_enrollment(user, url)
+        lms = cls.select_lms(url)
+
+        try:
+            return lms.get_enrollment(user, url)
+        except AttributeError:
+            raise ImproperlyConfigured()
 
     @classmethod
     def set_enrollment(cls, user, url):
@@ -37,4 +48,9 @@ class LMSHandler:
         Set enrollment for a user with a course run given its url. This method is a wrapper
         that routes the request to the LMS linked to this LMS.
         """
-        return cls.select_lms(url).set_enrollment(user, url)
+        lms = cls.select_lms(url)
+
+        try:
+            return lms.set_enrollment(user, url)
+        except AttributeError:
+            raise ImproperlyConfigured()
